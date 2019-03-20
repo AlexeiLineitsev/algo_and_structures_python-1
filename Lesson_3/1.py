37308c3e82d66400fcf29a2044735a16c407067e
alexei <alexei75chita@mail.ru>
2019-03-20 17:14:35 +0900

    Home work completed

--- Lesson_3/1.py
+++ Lesson_3/1.py
@@ -1,2 +1,19 @@
 # 1.	В диапазоне натуральных чисел от 2 до 99 определить,
 # сколько из них кратны каждому из чисел в диапазоне от 2 до 9.
+
+
+def summa_row(a, digit):
+    summ = 0
+    for i in a:
+        if i % digit == 0:
+            summ = summ + 1
+    return summ
+
+
+
+a = [i for i in range(2,100)]
+b = [i for i in range(2,10)]
+
+
+for i in b:
+    print('Числу', i,' в диапазоне натуральных чисел от 2 до 99 кратны ',summa_row(a,i), 'чисел')
--- Lesson_3/6.py
+++ Lesson_3/6.py
@@ -3,3 +3,32 @@
 между минимальным и максимальным элементами.
 Сами минимальный и максимальный элементы в сумму не включать.
 """
+
+def find_max_or_min_in_list(a, key):
+    max_a = a[0]
+    min_a = a[0]
+    index = 0
+    if key == 1:
+        for i in range(len(a)):
+            if max_a < a[i]:
+                max_a = a[i]
+                index = i
+        return index
+    else:
+        for i in range(len(a)):
+            if min_a > a[i]:
+                min_a = a[i]
+                index = i
+        return index
+
+a = [1, 2, 2, 3, 3, 3, 4, 4, 5, 5, 5, 5, 6, 7, 8, 9]
+
+max_index = find_max_or_min_in_list(a,1)
+min_index = find_max_or_min_in_list(a,0)
+
+summ = 0
+
+for i in range(min_index + 1, max_index):
+    summ = summ + a[i]
+
+print('Сумма элементов, за исключением максимально и минимального ',summ)
