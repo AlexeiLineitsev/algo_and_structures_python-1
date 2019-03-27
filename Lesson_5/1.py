6ffa9d1ad7bb9a49bf67473418a480e36d1cf5b1
alexei <alexei75chita@mail.ru>
2019-03-27 16:31:17 +0900

    Completed

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,34 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+import collections
+
+count_company = int(input('Желаемое количество компаний - '))
+
+companys = collections.defaultdict(list)
+
+s = 0
+
+for i in range(count_company):
+
+    name_company = input('Имя компании - ')
+    profit_company = [int(input('Введите прибыль за {} квартал - '.format(j+1))) for j in range(4)]
+    companys[name_company] = sum(profit_company)
+    s += sum(profit_company)
+
+s = s / count_company
+print('Cредняя прибыль (за год для всех предприятий) = ', round(s,0 ))
+
+print('Прибыль выше среднего')
+for i in companys:
+    if companys[i] > s:
+        print(i)
+
+print('Прибыль ниже среднего')
+for i in companys:
+    if companys[i] < s:
+        print(i)
+
+
+
+
--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,23 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+
+
+import collections
+import functools
+
+number = collections.defaultdict(list)
+
+for i in range(2):
+    n = str(input('Ваше шестнадцатеричное число - '))
+    number [i+1] = list(n)
+
+print(number)
+
+sum = sum([int(''.join(i), 16) for i in number.values()])
+
+print('Сумма', sum)
+
+sum = functools.reduce(lambda a, b: a * b, [int(''.join(i), 16) for i in number.values()])
+
+print('Произведение', sum)
